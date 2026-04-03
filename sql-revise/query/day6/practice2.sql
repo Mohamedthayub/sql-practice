09c6b6b411e7fada5a0cee6ce76f57cc9d4de4f0
buyath <buyaththayub@gmail.com>
2026-04-03 22:39:45 +0000

    joins done

---
+++ sql-revise/query/day6/practice2.sql
@@ -0,0 +1,11 @@
+USE PRACTICE;
+-- HERE WE  ARE GOING TO LEARN ABOUT ON DELETE 
+/*
+🧠 What is ON DELETE in SQL?
+
+👉 ON DELETE is a rule used with a FOREIGN KEY
+👉 It tells SQL:
+
+“What should happen to child data when parent data is deleted?”
+*/
+
